df5685d5a1afeda11272b8a67e2374a46af45e6d
aiktb <hey@aiktb.com>
2023-08-03 20:11:59 +0800

    refactor: migrating reusable logic to core.ts

--- src/contents/twitter.ts
+++ src/contents/twitter.ts
@@ -1,12 +1,6 @@
 import type { PlasmoCSConfig } from 'plasmo'
-import { toHiragana, toRomaji } from 'wanakana'
 
-import { sendToBackground } from '@plasmohq/messaging'
-import { Storage } from '@plasmohq/storage'
-
-import { Change, Class, type Furigana } from '~contents/core'
-import type { KurokanjiToken, KuromojiToken } from '~contents/kanji'
-import { toKurokanjiToken } from '~contents/kanji'
+import { addFurigana, Class, getAllTextNodes } from '~contents/core'
 
 export const config: PlasmoCSConfig = {
   matches: ['https://twitter.com/*'],
@@ -24,78 +18,7 @@ const observer = new MutationObserver((records, _observer) => {
       return getAllTextNodes(node)
     })
 
-  const uniqueJaNodes = [...new Set(jaNodes)]
-  addFurigana(uniqueJaNodes)
+  addFurigana(jaNodes)
 })
 
 observer.observe(document.body, { childList: true, subtree: true })
-
-const getAllTextNodes = (node: Node): Node[] => {
-  const textNodes: Node[] = []
-  if (node.nodeType === Node.TEXT_NODE) {
-    const textContent = node.textContent?.trim()
-    if (textContent && textContent.length) {
-      textNodes.push(node)
-    }
-  } else {
-    const nodes = Array.from(node.childNodes).flatMap(getAllTextNodes)
-    textNodes.push(...nodes)
-  }
-  return textNodes
-}
-
-// node must have only one text child node
-// <ruby>${token.original}<rt>${token.reading}</rt></ruby>
-const addFurigana = async (nodes: Node[]) => {
-  for (const node of nodes) {
-    const tokens: KurokanjiToken[] = await tokenize(node.textContent!)
-    // reverse() prevents the range from being invalidated
-    for (const token of tokens.reverse()) {
-      const ruby = await createRuby(token.original, token.reading)
-      const range = document.createRange()
-      range.setStart(node, token.start)
-      range.setEnd(node, token.end)
-      range.deleteContents()
-      range.insertNode(ruby)
-    }
-  }
-}
-
-const tokenize = async (text: string): Promise<KurokanjiToken[]> => {
-  const response = await sendToBackground<
-    { text: string },
-    { message: KuromojiToken[] }
-  >({
-    name: 'fetchKuromoji',
-    body: { text }
-  })
-  return toKurokanjiToken(response.message)
-}
-
-const createRuby = async (
-  original: string,
-  reading: string
-): Promise<HTMLElement> => {
-  const rubyNode = document.createElement('ruby')
-  const originalTextNode = document.createTextNode(original)
-
-  const storage = new Storage()
-  const furiganaType: Furigana = await storage.get(Change.Furigana)
-  switch (furiganaType) {
-    case 'hiragana':
-      reading = toHiragana(reading)
-      break
-    case 'romaji':
-      reading = toRomaji(reading)
-      break
-    case 'katakana':
-      // token.reading default is katakana
-      break
-  }
-  const readingTextNode = document.createTextNode(reading)
-  const rt = document.createElement('rt')
-  rt.appendChild(readingTextNode)
-  rubyNode.appendChild(originalTextNode)
-  rubyNode.appendChild(rt)
-  return rubyNode
-}
