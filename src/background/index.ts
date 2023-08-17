9d30d9d86f82f95ee81d44727352b0e20228c4ad
aiktb <hey@aiktb.dev>
2023-08-17 10:46:30 +0800

    fix: restart chrome causes data to restore default

--- src/background/index.ts
+++ src/background/index.ts
@@ -5,6 +5,9 @@ import { defaultConfig } from '~util/core'
 const storage = new Storage()
 chrome.runtime.onInstalled.addListener(async () => {
   for (const key in defaultConfig) {
-    await storage.set(key, defaultConfig[key])
+    const oldValue = await storage.get(key)
+    if (!oldValue) {
+      await storage.set(key, defaultConfig[key])
+    }
   }
 })
