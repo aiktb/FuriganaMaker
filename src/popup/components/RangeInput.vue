65063c996db5a4dcf88f8070562d6adbb6c0619d
aiktb <hey@aiktb.dev>
2023-08-17 11:01:04 +0800

    feat: complete the InputRange component

---
+++ src/popup/components/RangeInput.vue
@@ -0,0 +1,144 @@
+<script setup lang="ts">
+import {
+  clamp,
+  useDraggable,
+  useElementBounding,
+  useMousePressed
+} from '@vueuse/core'
+import { computed, onMounted, ref, watch } from 'vue'
+
+const props = defineProps<{
+  modelValue: number
+  min: number
+  max: number
+}>()
+
+const emit = defineEmits<{
+  'update:modelValue': [modelValue: number]
+  change: []
+}>()
+const track = ref<HTMLElement | null>(null)
+const thumb = ref<HTMLElement | null>(null)
+const { left, right, top, bottom } = useElementBounding(track)
+const { width, height } = useElementBounding(thumb)
+
+const { x: useX } = useDraggable(thumb)
+const y = ref(0)
+const x = computed(() => {
+  return clamp(useX.value, left.value, right.value - width.value)
+})
+
+const newModelValue = computed(() => {
+  const trackWidth = right.value - left.value - width.value
+  const percent = (x.value - left.value) / trackWidth
+  return Math.round(percent * (props.max - props.min) + props.min)
+})
+
+const { pressed } = useMousePressed({ target: thumb })
+watch(pressed, (pressed) => {
+  if (!pressed) {
+    emit('update:modelValue', newModelValue.value)
+    emit('change')
+  }
+})
+
+enum KeyEvent {
+  Add,
+  Subtract,
+  Emit
+}
+const keyHandler = (type: KeyEvent) => {
+  switch (type) {
+    case KeyEvent.Add:
+      useX.value = clamp(useX.value + 1, left.value, right.value - width.value)
+      break
+    case KeyEvent.Subtract:
+      useX.value = clamp(useX.value - 1, left.value, right.value - width.value)
+      break
+    case KeyEvent.Emit:
+      emit('update:modelValue', newModelValue.value)
+      emit('change')
+      break
+  }
+}
+
+// [left, right, top, bottom, width, height]
+// are only available after the component is mounted.
+onMounted(() => {
+  const percent = (props.modelValue - props.min) / (props.max - props.min)
+  const trackWidth = right.value - left.value - width.value
+  useX.value = clamp(
+    percent * trackWidth + left.value,
+    left.value,
+    right.value - width.value
+  )
+  y.value = (bottom.value - top.value - height.value) / 2 + top.value
+})
+</script>
+
+<template>
+  <div class="range">
+    <div class="track" ref="track">
+      <div
+        class="thumb"
+        ref="thumb"
+        tabindex="0"
+        :style="{ left: `${x}px`, top: `${y}px` }"
+        @keydown.left="keyHandler(KeyEvent.Subtract)"
+        @keydown.right="keyHandler(KeyEvent.Add)"
+        @keydown.up="keyHandler(KeyEvent.Add)"
+        @keydown.down="keyHandler(KeyEvent.Subtract)"
+        @keyup="keyHandler(KeyEvent.Emit)"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.range {
+  display: flex;
+  align-items: center;
+  position: relative;
+  flex-grow: 1;
+  box-sizing: border-box;
+  height: 1.5rem;
+  text-align: left;
+  background-color: transparent;
+  border: none;
+  border-radius: 0.3rem;
+  transition: all 250ms;
+  padding: 0 0.5rem;
+}
+
+.range:hover {
+  background-color: var(--gray);
+}
+
+.track {
+  height: 12%;
+  width: 100%;
+  border-radius: 0.5rem;
+  background-color: currentColor;
+}
+
+.thumb {
+  position: fixed;
+  width: 0.8rem;
+  height: 0.8rem;
+  border-radius: 50%;
+  background-color: var(--blue);
+  cursor: pointer;
+  outline: none;
+}
+
+.thumb:focus::before {
+  content: '';
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  right: -2px;
+  bottom: -2px;
+  border: 2px solid black;
+  border-radius: 4px;
+}
+</style>
