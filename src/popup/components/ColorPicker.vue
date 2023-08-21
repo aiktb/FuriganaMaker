1bac3d9f04b5acf6c75b19b487e8973151e7bbdb
aiktb <hey@aiktb.dev>
2023-08-21 13:55:24 +0800

    feat: easy palette complete

--- src/popup/components/ColorPicker.vue
+++ src/popup/components/ColorPicker.vue
@@ -1,16 +1,42 @@
-Realize hover prompt box
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+import ResetIcon from 'data-text:@Icons/Reset.svg'
+
 const props = defineProps<{
   modelValue: string
 }>()
+
 const emit = defineEmits<{
   'update:modelValue': [modelValue: string]
   change: []
 }>()
+
+const set = (color: string) => {
+  emit('update:modelValue', color)
+  emit('change')
+}
+
+const color = ref('#0075ff')
+
+onMounted(() => {
+  if (props.modelValue !== 'currentColor') {
+    color.value = props.modelValue
+  }
+})
 </script>
 
 <template>
-  <div class="colorPicker"></div>
+  <div class="colorPicker">
+    <input type="color" class="color" v-model="color" @change="set(color)" />
+    <div
+      tabindex="0"
+      class="resetIcon"
+      v-html="ResetIcon"
+      @click="set('currentColor')"
+      @keyup.enter="set('currentColor')"
+    />
+  </div>
 </template>
 
 <style scoped>
@@ -18,13 +44,46 @@ const emit = defineEmits<{
   display: flex;
   align-items: center;
   position: relative;
-  flex-grow: 1;
-  box-sizing: border-box;
-  height: 1.5rem;
   text-align: left;
-  border: none;
-  border-radius: 0.3rem;
   transition: all 250ms;
   padding: 0 0.5rem;
+  gap: 0.7rem;
+}
+
+.color {
+  width: 1.2rem;
+  height: 1.2rem;
+  box-sizing: content-box;
+  padding: 0;
+  border-radius: 50%;
+  cursor: pointer;
+  appearance: none;
+  position: relative;
+  border: none;
+  transition: all 250ms;
+  background-color: transparent;
+}
+
+.color::-webkit-color-swatch-wrapper {
+  padding: 0;
+}
+
+.color::-webkit-color-swatch {
+  border: none;
+  border-radius: 50%;
+  box-shadow: none;
+}
+
+.resetIcon {
+  width: 1.3rem;
+  height: 1.3rem;
+  display: flex;
+  align-items: center;
+  border-radius: 50%;
+  cursor: pointer;
+}
+.resetIcon:hover {
+  color: var(--feature);
+  transition: all 250ms;
 }
 </style>
