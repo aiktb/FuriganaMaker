759cf65a6c37b1b6a06fa5043aeadd84a1e74ffb
aiktb <hey@aiktb.com>
2023-10-04 15:33:39 +0800

    refactor: simpler Rule structure

--- src/background/messages/getSelector.ts
+++ src/background/messages/getSelector.ts
@@ -6,12 +6,28 @@ import { Rule, Selector } from '~contents/core'
 const storage = new Storage({ area: 'local' })
 const handler: PlasmoMessaging.MessageHandler<
   { domain: string },
-  { selectors: Selector[] }
+  { selector: Selector }
 > = async (req, res) => {
-  const rules: Rule[] = await storage.get('rules')
-  const ruleMap = new Map(rules.map((rule) => [rule.domain, rule.selector]))
-  const selectors: Selector[] = ruleMap.get(req.body!.domain) ?? []
-  res.send({ selectors })
+  const allRules: Rule[] = await storage.get('rules')
+  const validRules = allRules.filter(
+    (rule) => rule.domain === req.body!.domain && rule.enabled
+  )
+
+  const plain =
+    validRules
+      .filter((rule) => !rule.dynamic)
+      .map((rule) => rule.selector)
+      .join(', ') || undefined
+  const observer =
+    validRules
+      .filter((rule) => rule.dynamic)
+      .map((rule) => rule.selector)
+      .join(', ') || undefined
+  const selector: Selector = { plain, observer }
+
+  res.send({ selector })
+
+  // TODO: Add an event listener to reset the data structure here after the rule is edited.
 }
 
 export default handler
