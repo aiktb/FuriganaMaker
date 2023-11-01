cf34bc8fb629d7c0de269324f71f8914c2839313
aiktb <hey@aiktb.com>
2023-11-01 20:41:52 +0800

    fix(style): border-radius not uniform when focusing

--- src/popup/components/RangeInput.vue
+++ src/popup/components/RangeInput.vue
@@ -59,7 +59,7 @@ onMounted(() => {
 
 <template>
   <div
-    class="relative flex h-5 grow items-center px-2"
+    class="relative flex h-5 grow items-center px-2 rounded"
     tabindex="0"
     role="slider"
     :aria-valuemin="props.min"
@@ -72,10 +72,10 @@ onMounted(() => {
     @keydown.down="keyHandler(KeyEvent.Subtract)"
     @keyup="keyHandler(KeyEvent.Emit)"
   >
-    <div ref="track" class="h-[3px] w-full rounded-lg bg-current">
+    <div ref="track" class="h-[3px] w-full rounded-lg bg-current ">
       <div
         ref="slider"
-        class="fixed h-3.5 w-3.5 cursor-pointer rounded-full bg-[--feature-color] [transform:translate(-50%,-50%)]"
+        class="fixed h-3.5 w-3.5 cursor-pointer  rounded-full bg-[--feature-color] [transform:translate(-50%,-50%)]"
         :style="style"
       />
     </div>
