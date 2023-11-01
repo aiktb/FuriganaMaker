a408a499c26ea8c3cbbecf237c43d29ea33bc1c2
aiktb <hey@aiktb.com>
2023-11-01 12:36:28 +0800

    feat: initial completion of backend UI

---
+++ src/options/components/UserGuide.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+  <header
+    class="mb-4 rounded-md p-4 outline outline-2 outline-slate-600 dark:bg-slate-900"
+  >
+    <h1 class="my-2 flex items-center text-2xl font-bold text-sky-400">
+      User Guide
+      <Icon class="ml-2 inline-block h-6 w-6" icon="carbon:information" />
+    </h1>
+    <p class="py-1">
+      Editing User Rule requires mastering basic
+      <a
+        href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_selectors"
+        target="_blank"
+        class="text text-sky-400 underline transition-all hover:text-sky-600"
+      >
+        CSS selector</a
+      >
+      syntax, which may render the extension invalid, so please operate with
+      caution.
+    </p>
+    <p class="py-1">
+      Turn on the flag
+      <code class="rounded bg-slate-800 px-1 text-sky-400">dynamic</code>
+      and Furigana Maker will track newly loaded content in the page, which is
+      necessary for dynamically loaded websites such as Twitter.
+    </p>
+    <p class="py-1">
+      Set
+      <code class="rounded bg-slate-800 px-1 text-sky-400">enable="false"</code>
+      to disable the corresponding rule.
+    </p>
+    <p class="py-1">
+      In textarea you can use
+      <kbd class="rounded bg-slate-800 px-1 text-sky-400">ctrl + s</kbd>
+      to save the settings.
+    </p>
+  </header>
+</template>
--- src/options/index.vue
+++ src/options/index.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
 import '../popup/tailwind.css'
+
+import ItemMessage from './components/ItemMessage.vue'
+import JsonEditor from './components/JsonEditor.vue'
+import UserGuide from './components/UserGuide.vue'
 </script>
 
 <template>
-  <main></main>
+  <div class="w-full dark:bg-slate-800 dark:text-slate-200">
+    <div class="mx-auto min-h-screen w-full max-w-3xl p-2 text-lg">
+      <UserGuide />
+      <main>
+        <Suspense>
+          <JsonEditor />
+        </Suspense>
+      </main>
+      <ItemMessage />
+    </div>
+  </div>
 </template>
