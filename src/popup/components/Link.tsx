4921cad4b692013e3e6350433a9a4b208df540ef
aiktb <hey@aiktb.dev>
2024-01-25 22:33:28 +0800

    style: change link color to blue
    Former-commit-id: 4fe27de8fc06959126d13cfdf0497bd325291b56

--- src/popup/components/Link.tsx
+++ src/popup/components/Link.tsx
@@ -11,7 +11,7 @@ export default function Link({ href, text }: LinkProps) {
       target="_blank"
       href={href}
       rel="noopener noreferrer"
-      className="flex grow items-center gap-x-1 rounded px-2 capitalize"
+      className="flex grow items-center gap-x-1 rounded px-2 capitalize transition duration-300 hover:text-primary focus-visible:text-primary"
     >
       {text}
       <Icon aria-hidden="true" icon="ci:link" />
