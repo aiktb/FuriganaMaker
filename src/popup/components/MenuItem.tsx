73e2d976cba68f1dd1c31dc2d8b542b543c49119
aiktb <hey@aiktb.dev>
2024-01-27 20:58:22 +0800

    refactor: use `floating-ui` to optimize tooltips
    Former-commit-id: dd57beac20145c5dc4641550753d919cfed025d7

--- src/popup/components/MenuItem.tsx
+++ src/popup/components/MenuItem.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { flip, offset, useFloating } from '@floating-ui/react';
+import { Transition } from '@headlessui/react';
+import React, { Fragment, useState } from 'react';
 
 interface MenuItemProps {
   children: React.ReactNode;
@@ -7,18 +9,47 @@ interface MenuItemProps {
 }
 
 export default function MenuItem({ children, icon, tip }: MenuItemProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const { refs, floatingStyles } = useFloating({
+    open: isOpen,
+    placement: 'top',
+    strategy: 'fixed',
+    middleware: [offset(6), flip()],
+  });
   return (
-    <li className="group relative flex items-center gap-x-2">
+    <li className="flex items-center gap-x-2">
       <div className="text-2xl" aria-hidden="true">
         {icon}
       </div>
-      <div className="peer relative flex grow rounded transition-all hover:bg-gray-200  dark:hover:bg-slate-700">
+      <div
+        ref={refs.setReference}
+        onPointerEnter={() => setIsOpen(true)}
+        onPointerLeave={() => setIsOpen(false)}
+        onFocus={() => setIsOpen(true)}
+        onBlur={() => setIsOpen(false)}
+        className="flex grow rounded transition-all hover:bg-gray-200  dark:hover:bg-slate-700"
+      >
         {children}
       </div>
       {tip && (
-        <div className="invisible absolute -top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 rounded bg-slate-900 px-2 text-slate-200 opacity-0 transition-opacity delay-200 duration-500 group-first:top-[150%] peer-focus-within:visible peer-focus-within:opacity-100 peer-hover:visible peer-hover:opacity-100 dark:bg-slate-200 dark:text-slate-800">
-          {tip}
-        </div>
+        <Transition
+          show={isOpen}
+          as={Fragment}
+          enter="transition-opacity ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity ease-in duration-300"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div
+            ref={refs.setFloating}
+            style={floatingStyles}
+            className="text-slate-20 inline-flex rounded bg-slate-900 px-2 text-white dark:bg-slate-200 dark:text-slate-800"
+          >
+            {tip}
+          </div>
+        </Transition>
       )}
     </li>
   );
