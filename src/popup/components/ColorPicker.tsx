3aab2bb8972766926b2c17f804b9e14d32ebaca3
aiktb <hey@aiktb.dev>
2024-03-31 17:17:02 +0800

    fix: ColorPicker component does not represent the HSV/HSL color space correctly (#31)
    * chore: update ignore file
    
    * fix: ColorPicker component does not represent the HSV/HSL color space correctly
    
    * chore: fix lint warning

--- src/popup/components/ColorPicker.tsx
+++ src/popup/components/ColorPicker.tsx
@@ -57,145 +57,35 @@ interface ColorPickerPanelProps {
 }
 
 function ColorPickerPanel({ color, children, onChange }: ColorPickerPanelProps) {
+  const hsv = tinycolor(color).toHsv();
+  const [hue, setHue] = useState(hsv.h);
+  const [saturationAndValue, setSaturationAndValue] = useState({ s: hsv.s, v: hsv.v });
   const [input, setInput] = useState(tinycolor(color).toHexString());
-  const presetColors = [
-    'black',
-    'white',
-    'violet',
-    'orange',
-    'gold',
-    'sienna',
-    'lime',
-    'springgreen',
-    'forestgreen',
-    'fuchsia',
-    'blueviolet',
-    'orangered',
-    'aquamarine',
-    'teal',
-    'royalblue',
-    'darkturquoise',
-    'silver',
-    'crimson',
-    'pink',
-    'lightskyblue',
-    'aqua',
-    'lightsalmon',
-    'paleturquoise',
-    'gray',
-    'tomato',
-  ];
+
   useEffect(() => {
-    setInput(tinycolor(color).toHexString());
-  }, [color]);
+    const newColor = tinycolor({
+      h: hue,
+      s: saturationAndValue.s,
+      v: saturationAndValue.v,
+    }).toHexString();
+    setInput(newColor);
+    onChange(newColor);
+  }, [hue, saturationAndValue, onChange]);
 
-  function clamp(value: number, min: number, max: number): number {
-    return Math.min(Math.max(value, min), max);
-  }
-
-  function handleSaturationCanvasPointerDown(event: React.PointerEvent<HTMLDivElement>) {
-    const saturationCanvas = event.currentTarget;
-    const { width, height, left, top } = saturationCanvas.getBoundingClientRect();
-    onChange(
-      tinycolor({
-        h: tinycolor(color).toHsv().h,
-        s: clamp((event.clientX - left) / width, 0, 1),
-        v: clamp(1 - (event.clientY - top) / height, 0, 1),
-      }).toHexString(),
-    );
-    saturationCanvas.setPointerCapture(event.pointerId);
-    saturationCanvas.addEventListener('pointermove', handlePointerMove);
-    saturationCanvas.addEventListener('pointerup', handlePointerUp);
-
-    function handlePointerMove(event: PointerEvent) {
-      onChange(
-        tinycolor({
-          h: tinycolor(color).toHsv().h,
-          s: clamp((event.clientX - left) / width, 0, 1),
-          v: clamp(1 - (event.clientY - top) / height, 0, 1),
-        }).toHexString(),
-      );
-    }
-    function handlePointerUp() {
-      saturationCanvas.releasePointerCapture(event.pointerId);
-      saturationCanvas.removeEventListener('pointermove', handlePointerMove);
-      saturationCanvas.removeEventListener('pointerup', handlePointerUp);
-    }
-  }
-
-  function handleHueCanvasPointerDown(event: React.PointerEvent<HTMLDivElement>) {
-    const hueCanvas = event.currentTarget;
-    const { width, left } = hueCanvas.getBoundingClientRect();
-    onChange(
-      tinycolor({
-        h: clamp(((event.clientX - left) / width) * 360, 0, 359),
-        s: tinycolor(color).toHsv().s,
-        v: tinycolor(color).toHsv().v,
-      }).toHexString(),
-    );
-    hueCanvas.setPointerCapture(event.pointerId);
-    hueCanvas.addEventListener('pointermove', handlePointerMove);
-    hueCanvas.addEventListener('pointerup', handlePointerUp);
-    function handlePointerMove(event: PointerEvent) {
-      onChange(
-        tinycolor({
-          h: clamp(((event.clientX - left) / width) * 360, 0, 359),
-          s: tinycolor(color).toHsv().s,
-          v: tinycolor(color).toHsv().v,
-        }).toHexString(),
-      );
-    }
-    function handlePointerUp() {
-      hueCanvas.releasePointerCapture(event.pointerId);
-      hueCanvas.removeEventListener('pointermove', handlePointerMove);
-      hueCanvas.removeEventListener('pointerup', handlePointerUp);
-    }
+  function updateHSV(color: string) {
+    const hsv = tinycolor(color).toHsv();
+    setHue(hsv.h);
+    setSaturationAndValue({ s: hsv.s, v: hsv.v });
   }
   return (
     <div className="flex size-full flex-col justify-between px-2.5 py-3">
-      <div className="relative aspect-[7/6] w-full cursor-crosshair rounded-sm">
-        <div
-          onPointerDown={handleSaturationCanvasPointerDown}
-          className="absolute inset-0 rounded-sm shadow-inner"
-          style={{
-            background: `linear-gradient(to right, white, ${tinycolor({
-              h: tinycolor(color).toHsv().h,
-              s: 100,
-              v: 100,
-            }).toHexString()})`,
-          }}
-        >
-          <div className="absolute inset-0 rounded-sm bg-gradient-to-b from-transparent to-black" />
-          {/* Saturation and value picker */}
-          <div
-            className="absolute size-1 -translate-x-1/2 -translate-y-1/2 rounded-full "
-            style={{
-              boxShadow:
-                'rgb(255, 255, 255) 0px 0px 0px 1.5px, rgba(0, 0, 0, 0.3) 0px 0px 1px 1px inset, rgba(0, 0, 0, 0.4) 0px 0px 1px 2px',
-              left: `${tinycolor(color).toHsv().s * 100}%`,
-              top: `${100 - tinycolor(color).toHsv().v * 100}%`,
-            }}
-          />
-        </div>
-      </div>
+      <SaturationAndValuePicker
+        color={{ s: saturationAndValue.s, v: saturationAndValue.v }}
+        hue={hue}
+        onChange={setSaturationAndValue}
+      />
       <div className="flex gap-1">
-        <div
-          onPointerDown={handleHueCanvasPointerDown}
-          className="relative h-4 flex-1 cursor-crosshair rounded-sm"
-          style={{
-            background:
-              'linear-gradient(to right, rgb(255, 0, 0) 0%, rgb(255, 255, 0) 17%, rgb(0, 255, 0) 33%, rgb(0, 255, 255) 50%, rgb(0, 0, 255) 67%, rgb(255, 0, 255) 83%, rgb(255, 0, 0) 100%)',
-          }}
-        >
-          {/* Hue picker */}
-          <div
-            className="absolute top-1/2 h-3.5 w-1 -translate-x-1/2 -translate-y-1/2 rounded-[1px] bg-white "
-            style={{
-              boxShadow: 'rgba(0, 0, 0, 0.6) 0px 0px 2px',
-              left: `${(tinycolor(color).toHsv().h / 360) * 100}%`,
-            }}
-          />
-        </div>
+        <HuePicker hue={hue} onChange={setHue} />
         <div
           className="size-4 rounded-sm"
           style={{
@@ -217,7 +107,7 @@ function ColorPickerPanel({ color, children, onChange }: ColorPickerPanelProps) 
               }}
               onKeyDown={(event) => {
                 if (event.key === 'Enter' && tinycolor(input).isValid()) {
-                  onChange(input);
+                  updateHSV(input);
                 }
               }}
             />
@@ -225,7 +115,7 @@ function ColorPickerPanel({ color, children, onChange }: ColorPickerPanelProps) 
           <button
             className="flex h-6 items-center justify-center gap-0.5 rounded border-none px-1.5 font-sans shadow-sm outline-none ring-1 ring-gray-300 transition-all hover:text-primary focus-visible:text-primary focus-visible:ring-2 focus-visible:ring-primary dark:bg-slate-950 dark:ring-slate-700 dark:focus-visible:ring-primary"
             onClick={() => {
-              onChange('currentColor');
+              updateHSV('currentColor');
             }}
           >
             Reset
@@ -233,34 +123,154 @@ function ColorPickerPanel({ color, children, onChange }: ColorPickerPanelProps) 
           </button>
         </div>
       </div>
-      <div className="grid grid-cols-5 grid-rows-5 gap-2.5 border-t-2 border-gray-300 pt-3 dark:border-slate-700">
-        {presetColors.map((color) => {
-          const baseShadow = 'rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset';
-          const focusShadow = `${baseShadow} ,${color} 0px 0px 6px`;
-          return (
-            <button
-              key={color}
-              className="h-4 w-full cursor-pointer rounded-sm outline-offset-2"
-              style={{
-                boxShadow: baseShadow,
-                backgroundColor: color,
-              }}
-              onFocus={(event) => {
-                event.currentTarget.style.boxShadow = focusShadow;
-              }}
-              onBlur={(event) => {
-                event.currentTarget.style.boxShadow = baseShadow;
-              }}
-              onClick={() => {
-                onChange(color);
-              }}
-            >
-              <span className="sr-only">{color}</span>
-            </button>
-          );
-        })}
-      </div>
+      <ColorSwitcher
+        onChange={(color) => {
+          updateHSV(color);
+        }}
+      />
       {children}
+    </div>
+  );
+}
+
+function addPointerEventListener(
+  element: HTMLElement,
+  pointerId: number,
+  listener: (event: PointerEvent) => void,
+) {
+  element.setPointerCapture(pointerId);
+  element.addEventListener('pointermove', listener);
+  element.addEventListener(
+    'pointerup',
+    () => {
+      element.releasePointerCapture(pointerId);
+      element.removeEventListener('pointermove', listener);
+    },
+    { once: true },
+  );
+}
+
+function clamp(value: number, min: number, max: number): number {
+  return Math.min(Math.max(value, min), max);
+}
+
+interface HS {
+  s: number;
+  v: number;
+}
+
+interface SaturationAndValuePickerProps {
+  color: HS;
+  hue: number;
+  onChange: (color: HS) => void;
+}
+
+function SaturationAndValuePicker({ color, hue, onChange }: SaturationAndValuePickerProps) {
+  function handleSaturationCanvasPointerDown(event: React.PointerEvent<HTMLDivElement>) {
+    const saturationCanvas = event.currentTarget;
+    const { width, height, left, top } = saturationCanvas.getBoundingClientRect();
+    updateSaturationAndValue(event);
+    addPointerEventListener(saturationCanvas, event.pointerId, updateSaturationAndValue);
+    function updateSaturationAndValue(event: React.PointerEvent | PointerEvent) {
+      onChange({
+        s: clamp((event.clientX - left) / width, 0, 1),
+        v: clamp(1 - (event.clientY - top) / height, 0, 1),
+      });
+    }
+  }
+
+  return (
+    <div className="relative aspect-[7/6] w-full cursor-crosshair rounded-sm">
+      <div
+        onPointerDown={handleSaturationCanvasPointerDown}
+        className="absolute inset-0 rounded-sm shadow-inner"
+        style={{
+          background: `linear-gradient(to right, white, ${tinycolor({ h: hue, s: 100, v: 100 }).toHexString()})`,
+        }}
+      >
+        <div className="absolute inset-0 rounded-sm bg-gradient-to-b from-transparent to-black" />
+        <div
+          className="absolute size-1 -translate-x-1/2 -translate-y-1/2 rounded-full "
+          style={{
+            boxShadow:
+              'rgb(255, 255, 255) 0px 0px 0px 1.5px, rgba(0, 0, 0, 0.3) 0px 0px 1px 1px inset, rgba(0, 0, 0, 0.4) 0px 0px 1px 2px',
+            left: `${color.s * 100}%`,
+            top: `${100 - color.v * 100}%`,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+interface HuePickerProps {
+  hue: number;
+  onChange: (hue: number) => void;
+}
+
+function HuePicker({ hue, onChange }: HuePickerProps) {
+  function handlePointerDown(event: React.PointerEvent<HTMLDivElement>) {
+    const hueCanvas = event.currentTarget;
+    const { width, left } = hueCanvas.getBoundingClientRect();
+    updateHue(event);
+    addPointerEventListener(hueCanvas, event.pointerId, updateHue);
+    function updateHue(event: React.PointerEvent | PointerEvent) {
+      onChange(clamp(((event.clientX - left) / width) * 360, 0, 359));
+    }
+  }
+  return (
+    <div
+      onPointerDown={handlePointerDown}
+      className="relative h-4 flex-1 cursor-crosshair rounded-sm"
+      style={{
+        background:
+          'linear-gradient(to right, rgb(255, 0, 0) 0%, rgb(255, 255, 0) 17%, rgb(0, 255, 0) 33%, rgb(0, 255, 255) 50%, rgb(0, 0, 255) 67%, rgb(255, 0, 255) 83%, rgb(255, 0, 0) 100%)',
+      }}
+    >
+      <div
+        className="absolute top-1/2 h-3.5 w-1 -translate-x-1/2 -translate-y-1/2 rounded-[1px] bg-white"
+        style={{
+          boxShadow: 'rgba(0, 0, 0, 0.6) 0px 0px 2px',
+          left: `${(hue / 360) * 100}%`,
+        }}
+      />
+    </div>
+  );
+}
+
+function ColorSwitcher({ onChange }: { onChange: (color: string) => void }) {
+  // prettier-ignore
+  const colors = [
+    'black', 'white', 'violet', 'orange', 'gold',
+    'sienna', 'lime', 'springgreen', 'forestgreen', 'fuchsia',
+    'blueviolet', 'orangered', 'aquamarine', 'teal', 'royalblue',
+    'darkturquoise', 'silver', 'crimson', 'pink', 'lightskyblue',
+    'aqua', 'lightsalmon', 'paleturquoise', 'gray', 'tomato',
+  ]
+  return (
+    <div className="grid grid-cols-5 grid-rows-5 gap-2.5 border-t-2 border-gray-300 pt-3 dark:border-slate-700">
+      {colors.map((color) => {
+        const baseShadow = 'rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset';
+        const focusShadow = `${baseShadow} ,${color} 0px 0px 6px`;
+        return (
+          <button
+            key={color}
+            className="h-4 w-full cursor-pointer rounded-sm outline-offset-2"
+            style={{ boxShadow: baseShadow, backgroundColor: color }}
+            onFocus={(event) => {
+              event.currentTarget.style.boxShadow = focusShadow;
+            }}
+            onBlur={(event) => {
+              event.currentTarget.style.boxShadow = baseShadow;
+            }}
+            onClick={() => {
+              onChange(color);
+            }}
+          >
+            <span className="sr-only">{color}</span>
+          </button>
+        );
+      })}
     </div>
   );
 }
