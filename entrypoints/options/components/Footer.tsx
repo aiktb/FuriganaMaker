5d300169d6bec511cf3bf7fc81d3daa42d47beca
aiktb <hey@aiktb.dev>
2024-07-07 14:04:38 +0800

    feat: add build tool info

--- entrypoints/options/components/Footer.tsx
+++ entrypoints/options/components/Footer.tsx
@@ -1,31 +1,30 @@
-import { Trans } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 
 const generateYear = () => {
   const year = new Date().getFullYear();
   return year === 2023 ? year : `2023-${year}`;
 };
 
+interface FooterLinkProps {
+  href: string;
+  content: string;
+}
+
+function FooterLink({ href, content }: FooterLinkProps) {
+  return (
+    <a
+      className="text-black font-mono font-bold dark:text-white underline decoration-solid  transition hover:text-sky-500"
+      href={href}
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      {content}
+    </a>
+  );
+}
+
 export default function Footer() {
-  const author = (
-    <a
-      className="text-sky-500 transition hover:text-sky-700"
-      href="https://github.com/aiktb"
-      target="_blank"
-      rel="noreferrer noopener"
-    >
-      {" aiktb"}
-    </a>
-  );
-  const license = (
-    <a
-      className="text-sky-500 transition hover:text-sky-700"
-      href="https://github.com/aiktb/FuriganaMaker/blob/main/LICENSE"
-      target="_blank"
-      rel="noreferrer noopener"
-    >
-      {" MIT "}
-    </a>
-  );
+  const { t } = useTranslation();
 
   return (
     <footer className="border-gray-200 border-t dark:border-slate-800">
@@ -35,14 +34,40 @@ export default function Footer() {
             <Trans
               i18nKey="footerCopyright"
               values={{ yearsRange: generateYear() }}
-              components={{ author }}
+              components={{
+                author: <FooterLink content="aiktb" href="https://github.com/aiktb" />,
+              }}
             />
           </span>
           <span className="block sm:inline">
-            <Trans i18nKey="footerLicense" components={{ license }} />
+            <Trans
+              i18nKey="footerLicense"
+              components={{
+                license: (
+                  <FooterLink
+                    content="MIT"
+                    href="https://github.com/aiktb/FuriganaMaker/blob/main/LICENSE"
+                  />
+                ),
+              }}
+            />
           </span>
-          <span className="block sm:inline">Build with WXT.</span>
+          <span className="block sm:inline">
+            <Trans
+              i18nKey="footerBuiltWith"
+              components={{
+                tool: <FooterLink content="WXT" href="https://wxt.dev" />,
+              }}
+            />
+          </span>
         </p>
+        <a
+          className="dark:hover:text-white flex justify-center items-center rounded-md size-9 hover:bg-gray-100 dark:hover:bg-slate-800 dark:text-white text-black"
+          href="https://github.com/aiktb/FuriganaMaker"
+        >
+          <span className="sr-only">{t("srGithub")}</span>
+          <i className="i-fa6-brands-github size-5" />
+        </a>
       </div>
     </footer>
   );
