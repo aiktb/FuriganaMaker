29efdc75e396b0a4fd36fdbef7f1525f7024d8a9
aiktb <hey@aiktb.dev>
2024-07-08 23:54:23 +0800

    test: add some test

--- __TESTS__/e2e/pages/popup.test.ts
+++ __TESTS__/e2e/pages/popup.test.ts
@@ -1,6 +1,16 @@
-import { test } from "vitest";
-test("open popup page", async () => {
-  const page = await browser.newPage();
-  await page.goto(`chrome-extension://${extensionId}/popup.html`);
-  await page.waitForSelector("#app");
+import { describe, expect, test } from "vitest";
+
+describe("Open extension popup page", () => {
+  test("Every time you open it, there will be a fallback due to the storage reading.", async () => {
+    const page = await browser.newPage();
+    await page.goto(`chrome-extension://${extensionId}/popup.html`);
+    // <Logo /> is fallback, promise is not resolved
+    await page.waitForSelector("#root > svg");
+    // promise is resolved
+    await page.waitForSelector("menu");
+  });
+  test("When the installation is complete, open the official website", async () => {
+    const pageURLs = (await browser.pages()).map((page) => page.url());
+    expect(pageURLs).toContain("https://furiganamaker.app/welcome");
+  });
 });
