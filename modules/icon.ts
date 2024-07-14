ed5c413913632ac914de495c286df5ec8c87e0df
aiktb <hey@aiktb.dev>
2024-07-14 17:01:44 +0800

    refactor: reduce duplicate code

--- modules/icon.ts
+++ modules/icon.ts
@@ -5,27 +5,19 @@ import sharp from "sharp";
 import { defineWxtModule } from "wxt/modules";
 
 export default defineWxtModule((wxt) => {
-  const sizes = [16, 32, 48, 128];
-  const destDir = path.resolve(wxt.config.outDir, "icons");
-
-  wxt.hooks.hook("build:publicAssets", () => {
+  wxt.hooks.hook("build:done", () => {
+    const sizes = [16, 32, 48, 128];
+    const destDir = path.resolve(wxt.config.outDir, "icons");
     fs.mkdirSync(destDir);
-    const logoInstance = sharp(path.resolve(__dirname, "../assets/icons/Logo.svg"));
+    const logo = sharp(path.resolve(__dirname, "../assets/icons/Logo.svg"));
     for (const size of sizes) {
       const destFile = path.resolve(destDir, `${size}.png`);
-      logoInstance.resize(size, size).png().toFile(destFile);
+      const png = logo.resize(size, size).png();
+      if (wxt.config.mode === "development") {
+        png.grayscale().toFile(destFile);
+      } else {
+        png.toFile(destFile);
+      }
     }
   });
-
-  if (wxt.config.mode === "development") {
-    wxt.hooks.hook("build:done", async () => {
-      const iconPaths = sizes.map((size) => path.resolve(destDir, `${size}.png`));
-
-      for (const iconPath of iconPaths) {
-        const icon = fs.readFileSync(iconPath);
-        const iconBuffer = await sharp(icon).grayscale().toBuffer();
-        fs.writeFileSync(iconPath, iconBuffer);
-      }
-    });
-  }
 });
