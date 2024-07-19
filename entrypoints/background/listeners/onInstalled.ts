b3c7fc954ffcab90ccf82238bbb910c4fdde2718
Jaguar Zhou <hey@aiktb.dev>
2024-07-19 22:04:40 +0800

    chore(dev): prevents frequent opening of new tabs in development mode (#167)

--- entrypoints/background/listeners/onInstalled.ts
+++ entrypoints/background/listeners/onInstalled.ts
@@ -2,7 +2,12 @@ import { ExtEvent } from "@/commons/constants";
 
 export const registerOnInstalled = () => {
   chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason === "install") {
+    // Prevents frequent opening of new tabs in development mode.
+    if (import.meta.env.MODE === "development") {
+      return;
+    }
+
+    if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
       chrome.tabs.create({ url: "https://furiganamaker.app/welcome" });
 
       // Setting the contextMenu must not be outside of `runtime.onInstalled`,
@@ -14,6 +19,8 @@ export const registerOnInstalled = () => {
         documentUrlPatterns: ["https://*/*"],
       };
       chrome.contextMenus.create(contextMenuItem);
+    } else if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
+      chrome.tabs.create({ url: chrome.runtime.getURL("options.html#/changelog") });
     }
   });
 };
