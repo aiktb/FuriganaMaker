b63f4e6c58ebf87da335897f4c3895f291bc81a9
aiktb <hey@aiktb.dev>
2024-07-21 22:12:56 +0800

    fix(dev): no context menu items are created in development mode

--- entrypoints/background/listeners/onInstalled.ts
+++ entrypoints/background/listeners/onInstalled.ts
@@ -2,25 +2,27 @@ import { ExtEvent } from "@/commons/constants";
 
 export const registerOnInstalled = () => {
   chrome.runtime.onInstalled.addListener((details) => {
-    // Prevents frequent opening of new tabs in development mode.
-    if (import.meta.env.MODE === "development") {
-      return;
-    }
-
-    if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-      chrome.tabs.create({ url: "https://furiganamaker.app/welcome" });
-
-      // Setting the contextMenu must not be outside of `runtime.onInstalled`,
-      // otherwise it will report an error for creating the contextMenu multiple times.
-      const contextMenuItem: chrome.contextMenus.CreateProperties = {
-        id: ExtEvent.AddFurigana,
-        title: chrome.i18n.getMessage("shortcutAddFurigana"),
-        contexts: ["page"],
-        documentUrlPatterns: ["https://*/*"],
-      };
-      chrome.contextMenus.create(contextMenuItem);
-    } else if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
-      chrome.tabs.create({ url: chrome.runtime.getURL("options.html#/changelog") });
+    switch (details.reason) {
+      case chrome.runtime.OnInstalledReason.INSTALL:
+        {
+          // Setting the contextMenu must not be outside of `runtime.onInstalled`,
+          // otherwise it will report an error for creating the contextMenu multiple times.
+          const contextMenuItem: chrome.contextMenus.CreateProperties = {
+            id: ExtEvent.AddFurigana,
+            title: chrome.i18n.getMessage("shortcutAddFurigana"),
+            contexts: ["page"],
+            documentUrlPatterns: ["https://*/*"],
+          };
+          chrome.contextMenus.create(contextMenuItem);
+          chrome.tabs.create({ url: "https://furiganamaker.app/welcome" });
+        }
+        break;
+      case chrome.runtime.OnInstalledReason.UPDATE: {
+        if (import.meta.env.MODE === "production") {
+          // Prevents frequent opening of new tabs in development mode.
+          chrome.tabs.create({ url: chrome.runtime.getURL("options.html#/changelog") });
+        }
+      }
     }
   });
 };
