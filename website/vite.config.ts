5595dccb21b7b9687a78a0f90a94fef022e505c8
aiktb <hey@aiktb.dev>
2024-09-05 02:24:36 -0400

    fix(type): vite CSS configuration occasionally reports error

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,15 +2,8 @@ import {
   vitePlugin as remix,
   cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
 } from "@remix-run/dev";
-import autoprefixer from "autoprefixer";
-import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind, autoprefixer],
-    },
-  },
   plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
 });
