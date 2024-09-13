bc5f1033d78716245ae14baeb4d9a2abeff9d028
Jaguar Zhou <hey@aiktb.dev>
2024-09-13 23:39:48 +0800

    revert: restore Traditional Chinese i18n files (#241)
    * revert: restore Traditional Chinese i18n files
    
    * chore: fix translation

--- commons/i18n.ts
+++ commons/i18n.ts
@@ -4,7 +4,8 @@ import { initReactI18next } from "react-i18next";
 import en from "@/assets/_locales/en/translation.json";
 import ja from "@/assets/_locales/ja/translation.json";
 import ko from "@/assets/_locales/ko/translation.json";
-import zh from "@/assets/_locales/zh/translation.json";
+import zhCN from "@/assets/_locales/zh-CN/translation.json";
+import zhTW from "@/assets/_locales/zh-TW/translation.json";
 
 import { ExtStorage } from "./constants";
 import { getMoreSettings } from "./utils";
@@ -12,7 +13,8 @@ import { getMoreSettings } from "./utils";
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
-    zh: { translation: zh },
+    "zh-CN": { translation: zhCN },
+    "zh-TW": { translation: zhTW },
     ja: { translation: ja },
     ko: { translation: ko },
   },
