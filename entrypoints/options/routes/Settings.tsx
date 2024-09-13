a2ece0f0b955d141f77f9df6dc5cf7a70cb0e916
Jaguar Zhou <hey@aiktb.dev>
2024-09-13 23:41:04 +0800

    style: add light mode color (#242)

--- entrypoints/options/routes/Settings.tsx
+++ entrypoints/options/routes/Settings.tsx
@@ -125,11 +125,11 @@ function SettingSwitch({
       <Switch
         checked={enabled}
         onChange={onChange}
-        className="group relative flex h-7 w-14 cursor-pointer rounded-full bg-white/10 p-1 transition-colors duration-200 ease-in-out focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white data-[checked]:bg-white/10"
+        className="group relative flex h-7 w-14 cursor-pointer rounded-full bg-slate-900/10 dark:bg-white/10 p-1 transition-colors duration-200 ease-in-out focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white dark:data-[checked]:bg-white/10 data-[checked]:bg-black/10"
       >
         <span
           aria-hidden="true"
-          className="pointer-events-none inline-block size-5 translate-x-0 rounded-full bg-white ring-0 shadow-lg transition duration-200 ease-in-out group-data-[checked]:translate-x-7"
+          className="pointer-events-none inline-block size-5 translate-x-0 rounded-full bg-slate-900 dark:bg-white ring-0 shadow-lg transition duration-200 ease-in-out group-data-[checked]:translate-x-7"
         />
       </Switch>
     </div>
