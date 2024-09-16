33e74bd35cd405270b6777b1160ff40d4192cfc7
aiktb <hey@aiktb.dev>
2024-09-16 17:11:54 +0800

    fix: chrome context menu missing "add furigana"

--- entrypoints/background/listeners/onInstalled.ts
+++ entrypoints/background/listeners/onInstalled.ts
@@ -4,19 +4,13 @@ export const registerOnInstalled = () => {
   browser.runtime.onInstalled.addListener((details) => {
     switch (details.reason) {
       case chrome.runtime.OnInstalledReason.INSTALL: {
-        // Setting the contextMenu must not be outside of `runtime.onInstalled`,
-        // otherwise it will report an error for creating the contextMenu multiple times.
-        const contextMenuItem: chrome.contextMenus.CreateProperties = {
-          id: ExtEvent.AddFurigana,
-          title: browser.i18n.getMessage("shortcutAddFurigana"),
-          contexts: ["page"],
-          documentUrlPatterns: ["https://*/*"],
-        };
-        browser.contextMenus.create(contextMenuItem);
-        browser.tabs.create({ url: "https://furiganamaker.app/welcome" });
+        createContentMenu();
         break;
       }
       case chrome.runtime.OnInstalledReason.UPDATE: {
+        // If the context menu is not re-registered upon update, the menu may become missing or out of date.
+        createContentMenu();
+
         // V1.2.3 -> major.minor.patch
         // Only open the Changelog page to prompt the user when a new feature is available.
         const [majorPrevVersion, minorPrevVersion] = details.previousVersion!.split(".");
@@ -32,3 +26,18 @@ export const registerOnInstalled = () => {
     }
   });
 };
+
+function createContentMenu() {
+  // Remove the contextMenu before creating it to avoid creating multiple contextMenus.
+  browser.contextMenus.remove(ExtEvent.AddFurigana);
+  // Setting the contextMenu must not be outside of `runtime.onInstalled`,
+  // otherwise it will report an error for creating the contextMenu multiple times.
+  const contextMenuItem: chrome.contextMenus.CreateProperties = {
+    id: ExtEvent.AddFurigana,
+    title: browser.i18n.getMessage("shortcutAddFurigana"),
+    contexts: ["page"],
+    documentUrlPatterns: ["https://*/*"],
+  };
+  browser.contextMenus.create(contextMenuItem);
+  browser.tabs.create({ url: "https://furiganamaker.app/welcome" });
+}
