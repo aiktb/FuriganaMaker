1c2af5ba33122edd1bd2f3c809210befce7cde38
Jaguar Zhou <ai.ourship@gmail.com>
2024-11-26 21:08:55 +0800

    fix: context menu item not showing on http pages (#306)
    * fix: context menu item not showing on http pages
    
    * refactor: eliminate duplicate code
    
    * chore: release 2.2.1
    
    Release-As: 2.2.1

--- entrypoints/background/listeners/onInstalled.ts
+++ entrypoints/background/listeners/onInstalled.ts
@@ -2,16 +2,23 @@ import { ExtEvent } from "@/commons/constants";
 
 export const registerOnInstalled = () => {
   browser.runtime.onInstalled.addListener((details) => {
+    // If the context menu is not re-registered upon update, the menu may become missing or out of date.
+    // Remove the contextMenu before creating it to avoid creating multiple contextMenus.
+    browser.contextMenus.removeAll();
+    const contextMenuItem: chrome.contextMenus.CreateProperties = {
+      id: ExtEvent.AddFurigana,
+      title: browser.i18n.getMessage("shortcutAddFurigana"),
+      contexts: ["page"],
+      documentUrlPatterns: ["http://*/*", "https://*/*"],
+    };
+    browser.contextMenus.create(contextMenuItem);
+
     switch (details.reason) {
       case chrome.runtime.OnInstalledReason.INSTALL: {
-        createContentMenu();
         browser.tabs.create({ url: "https://furiganamaker.app/welcome" });
         break;
       }
       case chrome.runtime.OnInstalledReason.UPDATE: {
-        // If the context menu is not re-registered upon update, the menu may become missing or out of date.
-        createContentMenu();
-
         // V1.2.3 -> major.minor.patch
         // Only open the Changelog page to prompt the user when a new feature is available.
         const [majorPrevVersion, minorPrevVersion] = details.previousVersion!.split(".");
@@ -27,17 +34,3 @@ export const registerOnInstalled = () => {
     }
   });
 };
-
-function createContentMenu() {
-  // Remove the contextMenu before creating it to avoid creating multiple contextMenus.
-  browser.contextMenus.removeAll();
-  // Setting the contextMenu must not be outside of `runtime.onInstalled`,
-  // otherwise it will report an error for creating the contextMenu multiple times.
-  const contextMenuItem: chrome.contextMenus.CreateProperties = {
-    id: ExtEvent.AddFurigana,
-    title: browser.i18n.getMessage("shortcutAddFurigana"),
-    contexts: ["page"],
-    documentUrlPatterns: ["https://*/*"],
-  };
-  browser.contextMenus.create(contextMenuItem);
-}
