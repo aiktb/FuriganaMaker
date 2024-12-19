26f326fee8bf0406e256afe4c0955b1776df0060
aiktb <ai.ourship@gmail.com>
2024-12-19 23:59:37 +0800

    refactor: split the project into a more independent monorepo

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,3 +1,6 @@
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
+
 import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import { defineConfig } from "vite";
 
@@ -5,4 +8,9 @@ import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
   plugins: [cloudflareDevProxy(), reactRouter()],
+  css: {
+    postcss: {
+      plugins: [tailwind, autoprefixer],
+    },
+  },
 });
