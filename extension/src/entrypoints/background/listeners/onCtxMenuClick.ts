5de55d0c8caf6d1e2b21ed86f2af8dd89cbe96ac
Brian Zhou <ai.ourship@gmail.com>
2026-05-24 17:11:37 +0800

    refactor: remove redundant variable

--- extension/src/entrypoints/background/listeners/onCtxMenuClick.ts
+++ extension/src/entrypoints/background/listeners/onCtxMenuClick.ts
@@ -1,9 +1,10 @@
-import { ExtEvent } from "@/commons/constants";
 import { sendMessage } from "@/commons/message";
+
+export const CONTEXT_MENU_ID = "addFurigana";
 
 export const registerOnCtxMenuClick = () => {
   browser.contextMenus.onClicked.addListener(async (info, tab) => {
-    if (info.menuItemId === ExtEvent.AddFurigana) {
+    if (info.menuItemId === CONTEXT_MENU_ID) {
       await sendMessage("addFurigana", undefined, tab!.id!);
     }
   });
--- extension/src/entrypoints/background/listeners/onInstalled.ts
+++ extension/src/entrypoints/background/listeners/onInstalled.ts
@@ -1,16 +1,16 @@
-import { ExtEvent } from "@/commons/constants";
+import { CONTEXT_MENU_ID } from "./onCtxMenuClick";
 
 export const registerOnInstalled = () => {
   browser.runtime.onInstalled.addListener((details) => {
     // If the context menu is not re-registered upon update, the menu may become missing or out of date.
     // Remove the contextMenu before creating it to avoid creating multiple contextMenus.
     browser.contextMenus.removeAll();
-    const contextMenuItem: Browser.contextMenus.CreateProperties = {
-      id: ExtEvent.AddFurigana,
+    const contextMenuItem = {
+      id: CONTEXT_MENU_ID,
       title: browser.i18n.getMessage("shortcutAddFurigana"),
       contexts: ["page"],
       documentUrlPatterns: ["http://*/*", "https://*/*"],
-    };
+    } satisfies Browser.contextMenus.CreateProperties;
     browser.contextMenus.create(contextMenuItem);
 
     if (details.reason === browser.runtime.OnInstalledReason.INSTALL) {
